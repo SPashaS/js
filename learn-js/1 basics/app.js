0ede708cd405c184d745fc02a8b80c04ba6e9657
SPavelS <SPavel.LD@gmail.com>
2022-11-07 22:53:50 +0300

    feat: 1 basics 1.3

--- learn-js/1 basics/app.js
+++ learn-js/1 basics/app.js
@@ -21,3 +21,11 @@
 // const birthday = '18.04.1982';
 // можно BIRTHDAY но не AGE
 // const age = someCode(birthday);
+
+
+// --------  3 - Шаблонные строки
+// 3.1 Работа с переменными
+// let name = "Ilya";
+// alert( `hello ${1}` ); // hello 1
+// alert( `hello ${"name"}` ); // hello name
+// alert( `hello ${name}` ); // hello Ilya
